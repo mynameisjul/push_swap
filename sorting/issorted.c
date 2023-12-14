0c7aaf833b5a95857cebd9305c38d27681ffad8c
mynameisjul <jublaye@hotmail.fr>
2023-12-14 18:00:13 +0100

    unsure i actually did any work today

--- sorting/issorted.c
+++ sorting/issorted.c
@@ -3,22 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   issorted.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
+/*   By: julieblaye <julieblaye@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 17:28:01 by jblaye            #+#    #+#             */
-/*   Updated: 2023/12/13 13:09:58 by jblaye           ###   ########.fr       */
+/*   Updated: 2023/12/14 16:53:13 by julieblaye       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-int	issorted(t_pile *p)
+int	issorted(t_pile *p, int rev)
 {
 	size_t	i;
 
 	i = 1;
+	multtab(p, rev);
 	while (i < p->len && p->tab[i] > p->tab[i - 1])
 		i++;
+	multtab(p, rev);
 	if (i == p->len)
 		return (1);
 	return (0);
--- sorting/sortingcost.c
+++ sorting/sortingcost.c
@@ -1,38 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   threesome.c                                        :+:      :+:    :+:   */
+/*   sortingcost.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: julieblaye <julieblaye@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/12 17:58:17 by jblaye            #+#    #+#             */
-/*   Updated: 2023/12/14 14:39:28 by julieblaye       ###   ########.fr       */
+/*   Created: 2023/12/14 17:26:21 by julieblaye        #+#    #+#             */
+/*   Updated: 2023/12/14 17:30:59 by julieblaye       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	threesome(t_pile *p)
+int	sortingcost(t_pile *pa, t_pile *pb)
 {
-	int	imax;
 
-	imax = imaxvalue(*p);
-	if (issorted(p) == 0)
-	{
-		if (imax == 0)
-		{
-			rotate(p);
-			ft_printf("ra\n");
-		}
-		if (imax == 1)
-		{
-			rrotate(p);
-			ft_printf("rra\n");
-		}
-		if (p->tab[0] > p->tab[1])
-		{
-			swap(p);
-			ft_printf("sa\n");
-		}
-	}
 }
