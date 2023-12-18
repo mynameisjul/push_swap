0e51b49f974a4e9c1223bd5db65329868c4c6fc2
Julie Blaye <jblaye@paul-f4Br7s4.clusters.42paris.fr>
2023-12-18 14:12:31 +0100

    push swap finished and correct

--- sorting/finalpos.c
+++ sorting/finalpos.c
@@ -6,34 +6,67 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 12:03:16 by julieblaye        #+#    #+#             */
-/*   Updated: 2023/12/18 10:16:27 by jblaye           ###   ########.fr       */
+/*   Updated: 2023/12/18 14:12:15 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+int	mintab(t_pile p)
+{
+	int	i;
+	int	imin;
+
+	i = 1;
+	imin = 0;
+	while (i < (int) p.len)
+	{
+		if (p.tab[i] < p.tab[imin])
+			imin = i;
+		i++;
+	}
+	return (imin);
+}
+
+int	maxtab(t_pile p)
+{
+	int	i;
+	int	imax;
+
+	i = 1;
+	imax = 0;
+	while (i < (int) p.len)
+	{
+		if (p.tab[i] > p.tab[imax])
+			imax = i;
+		i++;
+	}
+	return (imax);
+}
+
 int	finalpos(t_pile p, int n)
 {
 	int	i;
 	int	pos;
-	
+	int	imax;
+	int	imin;
+
+	imax = maxtab(p);
+	imin = mintab(p);
 	i = 1;
 	pos = 0;
-	while (i < (int) p.len)
+	if (n > p.tab[imax] || n < p.tab[imin])
+		pos = imin;
+	else
 	{
-		if (p.tab[i] > p.tab[pos])
-			pos = i;
-		i++;
-	}
-	if (n > p.tab[pos])
-		return ((pos + 1) % (int) p.len);
-	i = 1;
-	pos = 0;
-	while (i < (int) p.len)
-	{
-		if (p.tab[i] > n && p.tab[i - 1] < n)
-			pos = i;
-		i++;
+		if (n > p.tab[p.len - 1] && n < p.tab[0])
+			return (0);
+		while (i < (int) p.len)
+		{
+			if (n > p.tab[i - 1] && n < p.tab[i])
+				pos = i;
+			i++;
+		}
 	}
 	return (pos);
 }
--- sorting/mediansort.c
+++ sorting/mediansort.c
@@ -6,7 +6,7 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:07:21 by jblaye            #+#    #+#             */
-/*   Updated: 2023/12/18 11:56:37 by jblaye           ###   ########.fr       */
+/*   Updated: 2023/12/18 13:47:28 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	mediansort(t_pile *pa, t_pile *pb, int median)
 	{
 		push(pa, pb);
 		ft_printf("pb\n");
-		if (pb->tab[0] >= median && pb->len > 1)
+		if (pb->tab[0] >= median && pb->len > 3)
 		{
 			rotate(pb);
 			ft_printf("rb\n");
